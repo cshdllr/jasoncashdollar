cc99a53a8291d5ca46824fa7291aa8548facc08b
Jason Cashdollar <jason.cashdollar@gmail.com>
2025-12-12 16:05:48 -0700

    add google analytics

---
+++ analytics.js
@@ -0,0 +1,17 @@
+// Google Analytics 4
+(function() {
+    var GA_ID = 'G-ZFW20GEJGF';
+    
+    // Inject gtag.js script
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=' + GA_ID;
+    document.head.appendChild(script);
+    
+    // Initialize gtag
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    window.gtag = gtag;
+    gtag('js', new Date());
+    gtag('config', GA_ID);
+})();
